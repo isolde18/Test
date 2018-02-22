bd9b4caf1ec2fe28651d613a30829e8a6594a691
silvia16 <36601499+silvia16@users.noreply.github.com>
2018-02-22 10:01:37 -0500

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,19 @@
+
+
+
+
+score1= int(input("Enter score one "))
+
+score2= int(input("Enter score two "))
+
+score3= int(input("Enter score three "))
+
+average = (score1 + score2 +score3)/3
+if average >=95:
+    print("Congrats!!!")
+    
+
+
+
+
+
