cb56c70204d9dbfecb1d7c3accac2a338716b0e7
silvia16 <36601499+silvia16@users.noreply.github.com>
2018-02-27 14:25:22 -0500

    Add files via upload

---
+++ if_num==x_or_num==y.py
@@ -0,0 +1,15 @@
+#Statements
+#Logical Operators
+#Two Boolean expressions as operands
+
+
+
+               
+
+date=float(input("Enter today's date "))
+if date== 27.02 or date==02.27:
+     print("Pay rent")
+else:
+     print("Enter today's date")
+     
+
