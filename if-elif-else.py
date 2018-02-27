ba0e224ffd189ed34421931189075136526cb3ac
silvia16 <36601499+silvia16@users.noreply.github.com>
2018-02-27 13:29:02 -0500

    Add files via upload

---
+++ if-elif-else.py
@@ -0,0 +1,19 @@
+#Structure statement
+#if-elif-else
+#27.02.2018
+#Python
+
+#Derfine variables
+
+num = int(input("Enter num value "))
+
+if num ==1:
+    print ("One")
+elif num ==2:
+    print ("Two")
+elif num ==3:
+    print ("Three")
+else:
+    print ("Unknown")
+
+#Display results
