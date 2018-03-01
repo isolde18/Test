7d8612ea9b07dd9514b8fc9eaff36106ce854042
silvia16 <36601499+silvia16@users.noreply.github.com>
2018-03-01 10:24:38 -0500

    Add files via upload

---
+++ library_points.py
@@ -0,0 +1,24 @@
+
+#CTI110
+#1.03.2018
+#Book Club
+#Display the awarded points
+#Based on the number of books purchased each month
+
+
+
+
+
+num=int (input("Enter a number of purchased books "))
+
+if num == 0 or num == 1:
+   print ("You earn 0 score!")
+if num == 2 or num == 3:
+    print("You earn 5 points!")
+if num == 4 or num == 5:
+    print ("You earned 15 points!")
+if num ==6 or num == 7:
+    print ("You earned 30 points!")
+if num ==8 or num == 9:
+    print ("You earned 60 points!")
+    
