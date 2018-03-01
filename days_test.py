6c0ecd47cd30807aff0778c8a63306eb719fe1ae
silvia16 <36601499+silvia16@users.noreply.github.com>
2018-03-01 09:49:40 -0500

    Add files via upload

---
+++ days_test.py
@@ -0,0 +1,25 @@
+
+#CTI110
+#1.03.2018
+#Days 0f the week
+
+
+num =int (input(" Write a number "))
+
+if num ==1:
+    print   ("Monday")
+elif num ==2:
+    print   ("Tuesday")
+elif num ==3:
+    print   ("Wednesday")
+elif num ==4:
+    print   ("Thursday")
+elif num ==5:
+    print   ("Friday")
+elif num ==6:
+    print   ("Saturday")
+elif num ==7:
+    print   ("Sunday")
+
+else:
+    print   ("Error")
