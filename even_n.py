2fc002300a7e140727744a64605e97b9edf400a0
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-11 15:18:01 -0400

    Add files via upload

---
+++ even_n.py
@@ -0,0 +1,16 @@
+
+
+
+#Asks for a number
+#Print if it's even or odd
+
+number= float (input("Tell me a number: "))
+if number % 2 == 0:
+    print(int(number), "is even.")
+elif number % 2 ==1:
+    print (int(number), "is odd.")
+else:
+    print (number, "is very strange!")
+
+
+    
