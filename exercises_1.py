19005b47dedb89e7df3076828e5ecf1c19c0151c
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-11 18:02:44 -0400

    Add files via upload

---
+++ exercises_1.py
@@ -0,0 +1,21 @@
+
+
+#write a program that asks the user
+#to input his/her name
+#if they enter your name
+#say 'That's a nice name'
+#if they enter 'apple juice'
+#or 'orange juice'
+#tell them how you feel about them winks
+#otherwise tell them
+#'You have a nice name'
+name=(input("Your name: "))
+
+if name == "Pavlik":
+    print (" Wow, what a nice name")
+elif name == 'apple juice':
+    print (" are you a droid  ")
+elif name== "orange juice":
+    print ("what is your password")
+else:
+    print("You have a nice name")
