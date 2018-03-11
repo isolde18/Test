1795d929d6d1514eba381e34d2a8edaf142d0fb3
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-11 09:50:29 -0400

    Add files via upload

---
+++ more_loops.py
@@ -0,0 +1,12 @@
+
+
+a = 1
+s = 0
+print ('enter numbers to add to the sum ')
+print ('enter 0 to quit ')
+while a != 0:
+    print ('current sum:', s)
+    a = float (input('number? '))
+    s = s + a
+    print ('total sum =' ,s)
+    
