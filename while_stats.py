1b3d5fc95e416a910a938a1c4149873a61ba6347
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-11 09:31:20 -0400

    Add files via upload

---
+++ while_stats.py
@@ -0,0 +1,26 @@
+
+
+#First, set the initial value of the variable a to 0 (zero).
+
+a = 0
+
+while a < 10:
+    a = a + 1
+    print (a)
+
+#While the value of the variable is less than 10 do the following:
+#Increase the value of the variable a by 1, as in : a=a+1!
+#Print to screen what the present value of the variable a is.
+#REPEAT! Until the value value of the variable a is equal to 9!? See note.
+
+    #Note:
+    #The value of the variable a will 
+    #increase by one with each repeat,
+    #or loop, of the 'while' statemen BLOCK'
+    #ITERATION
+    #            --
+    #while a < 10:|
+    #    a=a+1|<--| [The while statement BLOCK ]
+    #    print (a)|
+    #            --
+    
