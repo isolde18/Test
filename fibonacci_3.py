26db64865676653301b24d1c3f9586ff8e69c887
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-11 10:46:31 -0400

    Add files via upload

---
+++ fibonacci_3.py
@@ -0,0 +1,21 @@
+
+
+
+#fibonacci method_3
+
+a = 0
+b = 1
+count = 0
+mXCOUNT = 20
+
+#ONCE LOOP IS STARTED WE STAY IN IT
+while count < mXCOUNT:
+    count += 1
+    olda = a
+    a = a+ b
+    b = olda
+    print (olda,end=" ")
+print()
+
+    
+
