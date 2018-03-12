d28d8a306494570a5ffda1849e3c912260a906d7
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-11 20:10:16 -0400

    Add files via upload

---
+++ exercise_4.py
@@ -0,0 +1,11 @@
+
+
+
+#write a program that asks for two numbers
+#if the sum of the numbers is greater than 100
+#print "That is a big number."
+
+number1=float(input("enter number1: "))
+number2=float(input ("enter number2: "))
+if number1 + number2 >100:
+    print("THAT IS A BIG NUMBER.")
