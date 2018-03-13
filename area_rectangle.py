b23736d303b28e845013feea47f1bb14e862e593
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-12 20:22:19 -0400

    Add files via upload

---
+++ area_rectangle.py
@@ -0,0 +1,27 @@
+
+
+#calculates a given rectangle area
+def hello():
+    print ('Hello!')
+def area (width, height):
+    return width * height
+def print_welcome (name):
+    print ('Welcome,',name)
+def positive_input (prompt):
+    number = float (input(prompt))
+    while number <= 0:
+        print ("Must be a positive number")
+        number=float(input(prompt))
+    return number
+name = input ('Your Name: ')
+hello()
+print_welcome(name)
+print()
+print('to find the area of a rectangle, ')
+print('ener the width and the height below.')
+print()
+
+w=positive_input ("Width: ")
+h=positive_input("Height: ")
+
+print('Width =', w, 'Height= ', h, 'so Area=', area (w,h))
