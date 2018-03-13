72c96fa76e47a313d0b8469d03ce367a2bae027d
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-12 20:03:01 -0400

    Add files via upload

---
+++ temperature_converter.py
@@ -0,0 +1,33 @@
+
+
+#temperature converter
+#Fahrenheit to Celsius
+#Celsius to Fahrenheit
+
+def print_options():
+    print("Options:")
+    print(" 'p' print options")
+    print (" 'c' convert from Celsius")
+    print(" 'f' convert from Fahrenheit")
+    print (" 'q' quit the program")
+
+def celsius_to_fahrenheit(c_temp):
+    return 9.0 /5.0 *c_temp + 32
+
+def fahrenheit_to_celsius(f_temp):
+    return (f_temp-32.0) * 5.0/9.0
+
+choice="p"
+while choice !="q":
+    if choice=="c":
+        c_temp= float(input("Celsius temperature: "))
+        print("Fahrenheit:", celsius_to_fahrenheit (c_temp))
+        choice=input ("option: ")
+    elif choice=="f":
+        f_temp=float(input("Fahrenheit temperature: "))
+        print ("Celsius:", fahrenheit_to_celsius(f_temp))
+        choice=input ("option: ")
+    elif choice=="p": #Alternatively choice != "q": so that print
+                     #when anything unexpected inputed
+        print_options()
+        choice=input("opton: ")
