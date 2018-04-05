f99ef105be5785b4a45cf77be2f308d39385687d
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-04-05 02:16:44 -0400

    Add files via upload

---
+++ zeus_attempt.py
@@ -0,0 +1,17 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("Hello Zeus")
+
+zeus = turtle.Turtle()
+zeus.color("blue")
+zeus.pensize(3)
+
+zeus.forward(50)
+zeus.left(120)
+zeus.forward(50)
+
+wn.mainloop()
+
+
+
