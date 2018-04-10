661a9bf1103535843e1c386793ef480a142d88e0
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-04-10 09:41:35 -0400

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,4 @@
+def test(a,b,c):
+    print('a=',a,'b=',b,'c=',c)
+
+test(2,b=3,c=4)    
---
+++ test3.py
@@ -0,0 +1,12 @@
+number=1
+
+def varnum():
+
+    global number
+
+    number=2
+    print(number)
+
+varnum()
+
+print(number)
